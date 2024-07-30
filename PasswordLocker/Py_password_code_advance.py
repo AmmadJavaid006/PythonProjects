d7b30c4a2fa6a8a6bc51f15fe7b99298e835cea9
AmmadJavaid006 <ammad.javaid420@gmail.com>
2024-07-30 23:43:03 +0500

    Changes

--- PasswordLocker/Py_password_code_advance.py
+++ PasswordLocker/Py_password_code_advance.py
@@ -1,4 +1,8 @@
 import re, pyperclip, sys
+from cryptography.fernet import Fernet
+
+key = b'vbBaf5kSdiwjHsB6gRhhz5Vh1f36VEUaDLy4akH9pc8='
+fernet_key = Fernet(key)
 
 def load_credentials():
     passwordfile = open("Passwords.txt", 'r')
@@ -8,16 +12,24 @@ def load_credentials():
     passwords = re.findall(r"Password:\s*(\S*)", readfile)
     return dict(zip(websites, passwords))
 
-def save_credentials(file_path, credentials):
-    passwordfile = open(file_path, 'w')
+def save_credentials(credentials):
+    passwordfile = open("Passwords.txt", 'w')
     for k, v in credentials.items():
         passwordfile.write(f"Website: {k}\n")
         passwordfile.write(f"Password: {v}\n\n")
     passwordfile.close()
 
 def changepass(webin, passin, credentials):
-    credentials[webin] = passin
-    save_credentials("Passwords.txt", credentials)
+    passin = passin.encode("utf-8")
+    finalpassin = fernet_key.encrypt(passin)
+    credentials[webin] = finalpassin.decode("utf-8")
+    save_credentials(credentials)
+
+def addpass(addpasskey, addpassval, credentials):
+    addpassval = addpassval.encode("utf-8")
+    values = fernet_key.encrypt(addpassval)
+    credentials[addpasskey] = values.decode("utf-8")
+    save_credentials(credentials)
 
 def displaypass(credentials):
     print("----------------------")
@@ -32,15 +44,17 @@ def displaykeys(credentials):
         print("--------------")
 
 def copypass(tocopy, credentials):
-    pyperclip.copy(credentials[tocopy])
-
+    values = credentials[tocopy]
+    values = values.encode("utf-8")
+    finalval = fernet_key.decrypt(values)
+    pyperclip.copy(finalval.decode("utf-8"))
 def masterpass():
-    credentials = load_credentials("Passwords.txt")
+    credentials = load_credentials()
     for tries in range(1, 4):
         pin = input("Enter The Master Code: ")
         if pin == "72364899**":
             for triesss in range(1, 4):
-                mode = input("Which Mode You Want To Use? COPY OR CHANGE: ")
+                mode = input("Which Mode You Want To Use? COPY OR CHANGE OR ADD: ")
                 if mode.lower() == "copy":
                     displaypass(credentials)
                     for countx in range(1, 4):
@@ -70,6 +84,19 @@ def masterpass():
                                 sys.exit()
                         else:
                             print("Name or Master Password Not Correct! Try Again!")
+                    break
+                elif mode.lower() == "add":
+                    addkey = input("What Will Be The Website Name: ")
+                    addvalue = input("What Will Be The Password: ")
+                    for f in range(1, 5):
+                        confirm = input("Enter Master Password Again: ")
+                        if confirm == "72364899**":
+                            addpass(addkey, addvalue, credentials)
+                            break
+                        else:
+                            print("Wrong Master Password!!")
+                            f += 1
+                            continue
                     break
                 else:
                     print(f'Enter Either "COPY" or "CHANGE". "{mode}" Is Not A Mode')
